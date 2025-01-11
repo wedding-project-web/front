6be559a466cf303f940cecc0aa7c65d2b431de8d
seo myeong jin <seomyeongjin99@gmail.com>
2025-01-12 01:31:39 +0900

    wip: 스페셜웨딩프로모션 구현중

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/common/Layout";
 import MainPage from "./pages/MainPage";
+import SpecialWeddingPromotionPage from "./pages/SpecialWeddingPromotionPage";
 import WeddingAndPartyPage from "./pages/WeddingAndPartyPage";
 
 const App = () => {
@@ -12,6 +13,10 @@ const App = () => {
           <Route element={<Layout />}>
             <Route path="/" element={<MainPage />} />
             <Route path="/wedding-hall" element={<WeddingAndPartyPage />} />
+            <Route
+              path="/wedding-promotion"
+              element={<SpecialWeddingPromotionPage />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
