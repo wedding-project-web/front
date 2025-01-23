702ad0b7b2c53bf270369c90f10bbaeb08148d13
seo myeong jin <seomyeongjin99@gmail.com>
2025-01-23 14:42:14 +0900

    fix: 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,20 +2,17 @@ import axios from "axios";
 
 // 서버 Url instance 세팅
 
-const host =
-  window.location.hostname === "localhost"
-    ? `${import.meta.env.VITE_APP_SERVER_URL}/community`
-    : 'community';
+// const host =
+//   window.location.hostname === "localhost"
+//     ? `${import.meta.env.VITE_APP_SERVER_URL}/community`
+//     : "community";
 
-export const communityInstance = axios.create({
-  baseURL: host
-});
+// export const communityInstance = axios.create({
+//   baseURL: host,
+// });
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_APP_SERVER_URL,
 });
 
-export default {
-  instance,
-  communityInstance
-};
+export default instance;
